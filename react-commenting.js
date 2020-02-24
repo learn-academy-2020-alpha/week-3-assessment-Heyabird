d8fa0ac0991d9ca5733e917572e7ae415babfbe7
Heya Kwon <heyabird@gmail.com>
2020-02-23 18:18:45 -0800

    first try

--- react-commenting.js
+++ react-commenting.js
@@ -7,19 +7,19 @@
 class App extends Component{
   constructor(){
     super()
-    // 1.
+    // 1.Declare userInput as a state with a value of an empty string
     this.state = {
       userInput: "",
     }
   }
 
   robot = (e) => {
-    // 2.
+    // 2.Make a function that takes in user input and saves it to state userInput
     this.setState({ userInput: e.target.value})
   }
 
   render(){
-    // 3.
+    // 3. Destructure userInput
     let { userInput } = this.state
 
     return(
@@ -28,16 +28,16 @@ class App extends Component{
 
         <h3>Enter text here:</h3>
         <input
-          {/* 4. */}
+          {/* 4. when User types, this function is called automatically*/}
           onChange={ this.robot }
-          {/* 5. */}
+          {/* 5. the value saves what the User inputs */}
           value={ userInput }
         />
 
         <div>
-          {/* 6. */}
+          {/* 6. This is where the three robots will display on the page*/}
           <GoodRobot
-            {/* 7. */}
+            {/* 7. Save the prop value of userInput and send it to the Good Robot component*/}
             userInput={ userInput }
           />
           <BadRobot
@@ -61,11 +61,11 @@ class GoodRobot extends Component{
       // 8.
       <div>
         <h3>Good Robot</h3>
-        {/* 9. */}
+        {/* 9. Write out what Good Robot will spit out using the userInput prop*/}
         <h4>I hear you saying { this.props.userInput }. Is that correct?</h4>
       </div>
     )
   }
 }
-// 10.
+// 10. export GoodRobot
 export default GoodRobot
